3a7746593b10a9cae4697f34eb45cfe565018699
siltonIT <140279319+siltonIT@users.noreply.github.com>
2023-12-09 02:09:30 +0300

    create bad user intarface

--- Task 3.5/Task 3.5.cpp
+++ Task 3.5/Task 3.5.cpp
@@ -14,42 +14,42 @@ int main()
 
     int result[AREA_TRIAN];
 
+    //это лучшее что я мог сделать со своим кодом
     int start_ind = 0;
     int down_border = BORDER - 1;
-    int colums; 
     int delta = 0;
     int sum = 0;
     int end_ind;
     int delta_border = 0;
     int res_ind;
-    int k = 0;
-    int d = 0;
-    int m = 0;
+    int delta_res_ind = 0;
+    int delta_start = 0;
     int delta_res = 0;
+    int delta_delta_res = 0;
 
     for (int i = 0; i < AREA_TRIAN; ++i)
     {
         end_ind = start_ind + BORDER - down_border;
-        res_ind = start_ind + k;
+        res_ind = start_ind + delta_start;
 
         for (int j = start_ind; j < end_ind; ++j)
         {
-            res_ind += delta_res;
+            res_ind += delta_res_ind;
             sum += matrix[j] * matrix[res_ind];
-            ++d;
-            delta_res = BORDER - d;
+            ++delta_res;
+            delta_res_ind = BORDER - delta_res;
         }
 
-        delta_res = 0;
-        d = m;
-        ++k;
+        delta_res_ind = 0;
+        delta_res = delta_delta_res;
+        ++delta_start;
         result[i] = sum;
         sum = 0;
 
         if (down_border == delta_border)
         {
-            ++m;
-            k = 0;
+            ++delta_delta_res;
+            delta_start = 0;
             start_ind += BORDER - delta;
             ++delta;
             down_border =  BORDER - 1; 
